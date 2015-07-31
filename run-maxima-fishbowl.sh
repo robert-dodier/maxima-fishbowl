197db7e3654f89eadc245ccbb8325b0089150b3e
Robert Dodier <robert_dodier@users.sourceforge.net>
2015-07-31 14:29:44 -0700

    Remove --lisp and --lisp-exec options.

--- run-maxima-fishbowl.sh
+++ run-maxima-fishbowl.sh
@@ -1,5 +1,5 @@
 export PYTHONPATH=/home/robert/by-others/github/ipython/ipython
 export PATH=/home/robert/playpen/github/fishbowl-repl:\
 /home/robert/clozure-cl-svn/clozure-cl-svn-1.10-linuxx86/ccl:$PATH
-python3 /home/robert/playpen/github/fishbowl-repl/run-fishbowl.py notebook --lisp=ccl --lisp-exec=lx86cl
+python3 /home/robert/playpen/github/fishbowl-repl/run-fishbowl.py notebook
 
