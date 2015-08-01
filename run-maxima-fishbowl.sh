a588511cec8081b8fe7bba527f3b0a0ae1ef7d36
Robert Dodier <robert_dodier@users.sourceforge.net>
2015-07-31 22:12:58 -0700

    Rename run-fishbowl.py to run-maxima-fishbowl.py to more accurately suggest its purpose.

--- run-maxima-fishbowl.sh
+++ run-maxima-fishbowl.sh
@@ -1,5 +1,5 @@
 export PYTHONPATH=/home/robert/by-others/github/ipython/ipython
 export PATH=/home/robert/playpen/github/fishbowl-repl:$PATH
-python3 /home/robert/playpen/github/fishbowl-repl/run-fishbowl.py notebook\
+python3 /home/robert/playpen/github/fishbowl-repl/run-maxima-fishbowl.py notebook\
  --maxima-fishbowl-exec=/home/robert/maxima/maxima-code/binary/binary-openmcl/maxima-fishbowl
 
