319174958219910ca705aeb8da7f7ea25160a754
Robert Dodier <robert_dodier@users.sourceforge.net>
2015-07-31 22:10:05 -0700

    Remove Clozure CL from PATH as it is no longer needed.

--- run-maxima-fishbowl.sh
+++ run-maxima-fishbowl.sh
@@ -1,6 +1,5 @@
 export PYTHONPATH=/home/robert/by-others/github/ipython/ipython
-export PATH=/home/robert/playpen/github/fishbowl-repl:\
-/home/robert/clozure-cl-svn/clozure-cl-svn-1.10-linuxx86/ccl:$PATH
+export PATH=/home/robert/playpen/github/fishbowl-repl:$PATH
 python3 /home/robert/playpen/github/fishbowl-repl/run-fishbowl.py notebook\
  --maxima-fishbowl-exec=/home/robert/maxima/maxima-code/binary/binary-openmcl/maxima-fishbowl
 
