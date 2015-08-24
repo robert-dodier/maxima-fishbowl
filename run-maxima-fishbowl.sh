174c00e906fceead63828a6cdb1e5451ac14a000
Robert Dodier <robert_dodier@users.sourceforge.net>
2015-08-24 11:19:47 -0700

    Maxima-Fishbowl is deprecated -- put a note pointing to Maxima-Jupyter in the Readme file and remove all other files.

--- run-maxima-fishbowl.sh
+++
@@ -1,5 +0,0 @@
-export PYTHONPATH=/home/robert/by-others/github/ipython/ipython
-export PATH=/home/robert/playpen/github/maxima-fishbowl:$PATH
-python3 /home/robert/playpen/github/maxima-fishbowl/run-maxima-fishbowl.py notebook\
- --maxima-fishbowl-exec=/home/robert/maxima/maxima-code/binary/binary-openmcl/maxima-fishbowl
-
--- src/config.lisp
+++
@@ -1,5 +0,0 @@
-(in-package #:fishbowl)
-
-(defparameter +KERNEL-IMPLEMENTATION-NAME+ "Fishbowl")
-(defparameter +KERNEL-IMPLEMENTATION-VERSION+ "0.5")
-(defparameter +KERNEL-PROTOCOL-VERSION+ "4.1")
